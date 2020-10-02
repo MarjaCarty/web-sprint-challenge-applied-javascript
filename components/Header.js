5be8cd5fc6d36b8a51b719402c3e48bf5cfdf3a9
MarjaCarty <marja.d.carty@gmail.com>
2020-10-02 10:19:29 -0500

    Build header component

--- components/Header.js
+++ components/Header.js
@@ -11,4 +11,23 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header() {
+  const header = document.createElement("div");
+  const date = document.createElement("span");
+  const title = document.createElement("h1");
+  const temperature = document.createElement("span");
+
+  header.classList.add("header");
+  date.classList.add("date");
+  temperature.classList.add("temp");
+
+  date.textContent = "March 28, 2020".toUpperCase();
+  title.textContent = "Lambda Times";
+  temperature.textContent = "98°";
+
+  header.appendChild(date);
+  header.appendChild(title);
+  header.appendChild(temperature);
+
+  return header;
+}
